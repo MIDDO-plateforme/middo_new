4488d02bee59f1e1a9acf560cf8bfe77aa868266
Koba Lokota <bmbane@yahoo.fr>
2025-11-02 18:28:25 +0100

    Edition de profil fonctionnelle
    - Formulaire ProfileEditType avec tous les champs
    - Route /profile/edit
    - Template avec accordéons Bootstrap
    - Pré-remplissage des données
    - Sauvegarde et message flash de succès
    - Bouton Modifier mon profil sur la page profil
    
    Testé avec succès : Ville changée de Lyon à Paris

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileEditType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'user' => $this->getUser(),
+        return $this->render('profile/index.html.twig');
+    }
+
+    #[Route('/profile/edit', name: 'app_profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileEditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre profil a été mis à jour avec succès !');
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
