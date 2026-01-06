735e506451808604844de3db9d26f5a59e4fd8fc
Koba Lokota <bmbane@yahoo.fr>
2026-01-06 15:48:35 +0100

    SESSION 25 - Phase 1: Add premium splash screen with music

---
+++ src/Controller/SplashController.php
@@ -0,0 +1,15 @@
+﻿<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SplashController extends AbstractController
+{
+    #[Route('/', name: 'app_splash')]
+    public function index(): Response
+    {
+        return $this->render('splash/index.html.twig');
+    }
+}
