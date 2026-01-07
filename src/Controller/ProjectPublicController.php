5db11e6ee3756ad01a5d89846e238fc3df3ed4aa
Koba Lokota <bmbane@yahoo.fr>
2026-01-07 13:43:18 +0100

    SESSION 25: Simplify ProjectPublicController for testing

--- src/Controller/ProjectPublicController.php
+++ src/Controller/ProjectPublicController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProjectPublicController extends AbstractController
 {
     #[Route('/projets-public', name: 'app_project_public', methods: ['GET'])]
-    public function index(ProjectRepository $projectRepository): Response
+    public function index(): Response
     {
-        $projects = $projectRepository->findAll();
-
-        return $this->render('project/index.html.twig', [
-            'projects' => $projects,
-        ]);
+        // Version de test simple - juste afficher du HTML brut
+        return new Response('<h1 style="color: #f4a261; font-family: Poppins;"> TEST MIDDO - Route fonctionne !</h1>');
     }
 }
