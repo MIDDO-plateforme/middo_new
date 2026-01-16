dac5b5a23233eb8b172c5f62a5f077466b21b0fe
Koba Lokota <bmbane@yahoo.fr>
2026-01-16 09:34:58 +0100

    Fix: Use Attribute instead of Annotation for Route in AuthController

--- src/Controller/Api/AuthController.php
+++ src/Controller/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api')]
 class AuthController extends AbstractController
