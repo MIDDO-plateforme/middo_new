115492a72863027991d3a6c4e65415a8102bc931
Baudouin <mbane.lokota@gmail.com>
2026-02-10 19:25:26 +0100

    Fix controller class name: IaLabController

--- src/Controller/IaLabController.php
+++ src/Controller/IaLabController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IaLogsController extends AbstractController
+class IaLabController extends AbstractController
 {
     #[Route('/ia/logs', name: 'ia_logs')]
     public function logs(): Response
